c3afdc937bf159896307e8b7aff68af5abee6321
Matthew Sullivan <143848513+Daceit@users.noreply.github.com>
2024-01-24 14:50:49 -0800

    made result appear screen

--- calculator.js
+++ calculator.js
@@ -1,6 +1,16 @@
 function calculator() {
     let calc1 = document.getElementsByName(`num1`)[0].value;
    let result = eval(calc1);
+   document.open()
+   document.write(`
+    <section>
+        <input type="text" id="num1" name="num1" class="num1">
+            <button id="calc1 calc2" onclick= "calculator()">calculate</button>
+        <p id="result"></p>
+    </section>`
+   )
+    document.write(result)
+   document.close()
    console.log(result);
 }
 calculator();
